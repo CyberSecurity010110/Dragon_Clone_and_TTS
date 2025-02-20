0f4f07b38427dae9b43046a8758de7fdf24aed11
Jayson Rivers <privesec010110@gmail.com>
2025-02-20 01:30:17 -0500

    Update generate_speech.py

--- output_handling/generate_speech.py
+++ output_handling/generate_speech.py
@@ -1,10 +1,4 @@
 def generate_speech(model, text_input, cloned_voice=None):
     # Use the cloned voice if provided, otherwise use the default voice
-    if cloned_voice is not None:
-        # Generate speech using the cloned voice
-        speech_output = model.generate_speech(text_input, cloned_voice)
-    else:
-        # Generate speech using the default voice
-        speech_output = model.generate_speech(text_input)
-    
+    speech_output = model.generate_speech(text_input, cloned_voice)
     return speech_output
